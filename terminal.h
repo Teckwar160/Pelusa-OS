dab9fe36c344a1c69f775518560fe581857837d3
Teckwar160 <teckwar160@gmail.com>
2018-10-07 15:18:56 -0500

    termianl modificada

--- terminal.h
+++ terminal.h
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
+#include <string.h>
 using namespace std;
+
+
+
+void comandos();
 
 void terminal(){
 	//Variables
@@ -19,5 +25,23 @@ void terminal(){
 			salida = true;
 		}
 
+		if (comando == "list"){
+			comandos();
+		}
+
 	}while(salida != true);
+}
+
+void comandos(){
+	ifstream archivo;
+	string texto;
+	archivo.open("comandos.txt",ios::in);
+	if(archivo.fail()){
+		cout << endl << "No se pudo abrir el archivo" << endl << endl;
+	}
+	while(!archivo.eof()){
+		getline(archivo,texto);
+		cout << texto << endl;
+	}
+	archivo.close();
 }
